788d7f6860f6650e85f422fee99d564e8ca0d142
Daniele <d.tarini@hyperborea.com>
2021-01-30 14:55:12 +0100

    Fading in debrief buttons

--- swordfight/src/app/components/main/fight-debrief/fight-debrief.component.ts
+++ swordfight/src/app/components/main/fight-debrief/fight-debrief.component.ts
@@ -1,9 +1,34 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-fight-debrief',
   templateUrl: './fight-debrief.component.html',
-  styleUrls: ['./fight-debrief.component.scss']
+  styleUrls: ['./fight-debrief.component.scss'],
+  animations: [
+    trigger('enterBottom', [
+      transition(':enter', [
+        style({opacity: 0, transform: 'translateY(100px)'}), 
+        animate('1000ms'),
+        style({opacity: 1}),
+      ]),
+    ]),
+    trigger('enterRight', [
+      transition(':enter', [
+        style({opacity: 0, transform: 'translateX(100px)'}), 
+        animate('1000ms'),
+        style({opacity: 1}),
+      ]),
+    ]),
+    trigger('enterLeft', [
+      transition(':enter', [
+        style({opacity: 0, transform: 'translateX(-100px)'}), 
+        animate('1000ms'),
+        style({opacity: 1}),
+      ]),
+    ]),
+
+  ],
 })
 export class FightDebriefComponent implements OnInit {
 
